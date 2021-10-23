c5f9e5a2992fe16e225a305ad32c922a94a44cd4
Catherine Butler <catherine.butler@softwareone.com>
2021-10-23 18:27:17 +0100

    second

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 student_list = []
 max_list = 30
 
+
+
 while len(student_list) < max_list:
     student_name = input("Please enter a name?:")
     student_list.append(student_name)
+    max_list = max_list - 1
+    print(max_list)
+
 
 if len(student_list) == max_list:
     print(student_list)
